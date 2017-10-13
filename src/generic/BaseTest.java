8123c97be278a0961da797a6c62d1e6ebbfd645d
javagalbhanu <javagalbhanu@gmail.com>
2017-10-13 10:05:05 -0700

    FrameWork Grid
    Grid is added to Framework
    Refer NoteSteps.txt

--- src/generic/BaseTest.java
+++ src/generic/BaseTest.java
@@ -1,11 +1,16 @@
 package generic;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public abstract class BaseTest implements IAutoConst{
     public WebDriver driver;
@@ -13,9 +18,13 @@ public abstract class BaseTest implements IAutoConst{
     	System.setProperty(CHROME_KEY,CHROME_VALUE);
     	System.setProperty(GECKO_KEY, GECKO_VALUE);
     }
+    @Parameters({"node","browser"})
 	@BeforeMethod(alwaysRun=true)
-	public void openApplication(){
-		driver=new ChromeDriver();
+	public void openApplication(String node,String browser) throws Exception{
+		URL remote=new URL(node);
+		DesiredCapabilities d=new DesiredCapabilities();
+		d.setBrowserName(browser);
+		driver=new RemoteWebDriver(remote,d);
 		String url = Lib.getProperty(CONFIG_PATH,"URL");
 		driver.get(url);
 		String sITO = Lib.getProperty(CONFIG_PATH,"ITO");
